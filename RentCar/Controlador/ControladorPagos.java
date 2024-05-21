f9b3ceead87ae919adedd4dbfe2b156ea63f644e
Alviz <juanse20092807@gmail.com>
2024-05-21 00:01:16 -0500

    Controladores

---
+++ RentCar/Controlador/ControladorPagos.java
@@ -0,0 +1,22 @@
+package Controlador;
+
+public class ControladorPagos {
+    private float precioTotalPagar;
+    private boolean descuentoPreferencia;
+
+    public float consultarPrecioServiciosAdicionales(){
+        float preciosAdicionales;
+        return preciosAdicionales;
+    }
+    public float calcularValorTotal(){
+        float valorTotal;
+        return valorTotal;
+    }
+    public boolean realizarPago(float precioTotalPagar){
+        boolean pagoExistoso=false;
+        return pagoExistoso;
+    }
+    public void realizarContrato(){
+
+    }
+}
---
+++ RentCar/Controlador/ControladorRegistro.java
@@ -0,0 +1,24 @@
+package Controlador;
+
+import Modelo.Usuario;
+
+public class ControladorRegistro {
+    private int telefono;
+    private String correoElectronico;
+    private String tipoUsuario;
+    private boolean validacion;
+
+    public void registrarUsuario(int telefono, String correoElectronico, String tipoUsuario){
+
+    }
+    public void AgregarALista(Usuario user, String tipoUsuario){
+
+    }
+    public boolean validarRegistro(){
+        boolean validar=false;
+        return validar;
+    }
+    public void darFechaEvaluacion(){
+
+    }
+}
