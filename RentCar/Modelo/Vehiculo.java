9ad2506d485af259aa5ce63bc3e39f57a960bb08
Andres <ahenao.2005@gmail.com>
2024-05-24 20:02:56 -0500

    Implementacion en Empresa Se cargan los archivos de texto

--- RentCar/Modelo/Vehiculo.java
+++ RentCar/Modelo/Vehiculo.java
@@ -55,6 +55,7 @@ public class Vehiculo {
         this.categoria = categoria;
         this.kitCarretera = kitCarretera;
     }
+
     public int getIdTarjetaDePropiedad() {
         return idTarjetaDePropiedad;
     }
